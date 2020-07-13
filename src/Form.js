ab011b0c7f0d93ac09e85b19cafb6306551e63bb
pranavchati <pchati2003@gmail.com>
2020-07-13 16:21:13 -0400

    Created bare files for form and login

---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class Form extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+
+                </form>
+            </div>
+        )
+    }
+}
