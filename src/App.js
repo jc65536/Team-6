f492824aa5bcc655c0e933f1e0f5e1da87ffe850
pranavchati <pchati2003@gmail.com>
2020-07-13 19:16:10 -0400

    Created new components for login and home, along with adding firebase authenticaton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import fire from "./config/fire";
 import './App.css';
+import {Home} from "./Home";
+import Login from "./Login";
 
-function App() {
-  return (
-    <div className="app">
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {}
+        }
+        this.authListener = this.authListener.bind(this);
+    }
 
-    </div>
-  );
+    componentDidMount() {
+        this.authListener();
+    }
+
+    authListener() {
+        fire.auth().onAuthStateChanged((user) => {
+            //console.log(user);
+            if (user) {
+                this.setState({user});
+               // localStorage.setItem('user', user.uid);
+            } else {
+                this.setState({user: null});
+                //localStorage.removeItem('user');
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.user ? (<Home/>) : (<Login/>)}
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+                <header>
+
+                </header>
+                <main>
+                    Hello
+                </main>
+                <footer>
+
+                </footer>
+            </div>
+        )
+    }
+}
