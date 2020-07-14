a58e67ed5a926df1f026ccab118914c07b26872d
Jason Cheng <jasoncheng224@gmail.com>
2020-07-14 00:09:56 -0700

    Working, but ugly, form

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Form from "./Form"
 import * as serviceWorker from './serviceWorker';
+import DatabaseTest from './DatabaseTest';
 
 ReactDOM.render(
-    <App />,document.getElementById('root')
+    <div>
+        <Form />
+    </div>
+    , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
