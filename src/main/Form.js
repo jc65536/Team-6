c0c51ae342cdc533124e70a912dcf0b7fc2594c4
pranavchati <pchati2003@gmail.com>
2020-07-14 08:27:01 -0400

    Added iframe(google form)

--- src/main/Form.js
+++ src/main/Form.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import '../App.css';
+
 
 class Form extends Component {
     render() {
@@ -7,7 +9,8 @@ class Form extends Component {
                 <form>
                     <iframe
                         src="https://docs.google.com/forms/d/e/1FAIpQLSfpQjmRZ3CwXlBPozepV-23gCxqudVpZmg0j7M8CBR1fYJxOQ/viewform?embedded=true"
-                        width="640" height="938" frameBorder="0" marginHeight="0" marginWidth="0">Loading…
+                        width="640" height="938" frameBorder="0" marginHeight="0" marginWidth="0"
+                        className={"form-google"}>Loading…
                     </iframe>
                 </form>
             </div>
