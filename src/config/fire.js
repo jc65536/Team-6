d24676f7038a53b24eaa9901eea716e8e5806b71
Jason Cheng <jasoncheng224@gmail.com>
2020-07-14 09:22:37 -0700

    fixed config/fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -11,5 +11,9 @@ var config = {
     // measurementId: "G-M8KVXCVNN9"
 };
 
-const fire = firebase.initializeApp(config);
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+const fire = firebase;
 export default fire;
